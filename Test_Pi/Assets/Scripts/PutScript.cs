eb54691a91cbdd8bd1704c7e6e7a7a101da8ecef
ksimaster <ksimasterlord@gmail.com>
2021-02-24 21:14:33 +0300

    Начало по ТЗ
    Реализация начала по ТЗ на фабрику

---
+++ Test_Pi/Assets/Scripts/PutScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutScript : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject clone;
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+                clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+        }
+    }
+
+}
